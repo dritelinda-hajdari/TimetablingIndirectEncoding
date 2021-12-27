2f6e1a7988a60e359629992ccafb880d9a001f15
dritelinda.hajdari <dritelinda.hajdari@asseco-see.com>
2021-12-27 20:01:01 +0100

    Added fixes

--- src/com/examination/timetabling/App.java
+++ src/com/examination/timetabling/App.java
@@ -2,7 +2,6 @@ package com.examination.timetabling;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class App 
@@ -10,30 +9,37 @@ public class App
     public static void main( String[] args )
     {
         int[] intialSolution = new int[] {4, 5, 13, 1, 1, 4, 13, 2};
-        String[][] examinationTimetabling = new String[4][4];
         String [] examTimetabling = new String[17];
         Arrays.fill(examTimetabling, "");
-        Map<String, String[]> sameStudentsInExams = new HashMap<>();
-        sameStudentsInExams.put("E1", new String[]{"E2","E4","E6"});
-        sameStudentsInExams.put("E2", new String[]{"E1"});
-        sameStudentsInExams.put("E4", new String[]{"E1","E5","E6","E8"});
-        sameStudentsInExams.put("E5", new String[]{"E4","E8"});
-        sameStudentsInExams.put("E6", new String[]{"E1","E4"});
-        sameStudentsInExams.put("E8", new String[]{"E4","E5"});
-        
+        Map<String, String[]> sameStudentsInExams = initializeConstraints();
         for (int i = 0; i < intialSolution.length; i++) {
         	int position = intialSolution[i];
         	int index = i + 1;
         	String exam = "E"+ index;
         	checkAndSetExam(examTimetabling, position, exam, sameStudentsInExams);
 		}
+        int dayIndex = 0;
+        String[] days = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday"};
         for (int i = 1; i < examTimetabling.length; i=i+4) {
-        	System.out.println("9:00" + examTimetabling[i]);
-        	System.out.println("11:00" + examTimetabling[i+1]);
-        	System.out.println("2:00" + examTimetabling[i+2]);
-        	System.out.println("4:00" + examTimetabling[i+3]);
+        	System.out.println("\n" + days[dayIndex]);
+        	++dayIndex;
+        	System.out.println("9:00 " + examTimetabling[i]);
+        	System.out.println("11:00 " + examTimetabling[i+1]);
+        	System.out.println("2:00 " + examTimetabling[i+2]);
+        	System.out.println("4:00 " + examTimetabling[i+3]);
 		}
     }
+
+	private static Map<String, String[]> initializeConstraints() {
+		Map<String, String[]> sameStudentsInExams = new HashMap<>();
+        sameStudentsInExams.put("E1", new String[]{"E2","E4","E6"});
+        sameStudentsInExams.put("E2", new String[]{"E1"});
+        sameStudentsInExams.put("E4", new String[]{"E1","E5","E6","E8"});
+        sameStudentsInExams.put("E5", new String[]{"E4","E8"});
+        sameStudentsInExams.put("E6", new String[]{"E1","E4"});
+        sameStudentsInExams.put("E8", new String[]{"E4","E5"});
+		return sameStudentsInExams;
+	}
     
     private static void checkAndSetExam(String[] examTimetabling, int position, String exam, Map<String, String[]> sameStudentsInExams) {
 		String[] sameExams = sameStudentsInExams.get(exam);
